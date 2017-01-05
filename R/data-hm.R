c278a794f38b789c1ec95a99685728bb74b80579
joethorley <joe@poissonconsulting.ca>
2017-01-05 14:49:01 -0800

    initial commit

---
+++ R/data-hm.R
@@ -0,0 +1,17 @@
+#' House martin annual counts
+#'
+#' The house martin (\emph{Delichon urbica}) population
+#' annual counts from Magden (a small village in Northern Switzerland)
+#' collected by Reto Freuler from 1990 to 2009.
+#'
+#' The variables are as follows:
+#' \itemize{
+#'   \item \code{hm} the count (integer).
+#'   \item \code{year} the year (integer).
+#' }
+#'
+#' @format A data frame with 20 rows and 2 columns
+#' @references
+#' Kery M & Schaub M (2011) Bayesian Population Analysis
+#' using WinBUGS. Academic Press. (\url{http://www.vogelwarte.ch/bpa})
+"hm"
---
+++ R/data-peregrine.R
@@ -0,0 +1,19 @@
+#' Peregrine falcon breeding population data
+#'
+#' The peregrine falcon (\emph{Falco peregrinus}) population
+#' breeding in the French Jura from 1964 to 2003.
+#'
+#'  The variables are as follows:
+#' \itemize{
+#'   \item \code{Year} the year (integer).
+#'   \item \code{Pairs} the number of adult pairs (integer).
+#'   \item \code{R.pairs} the number of reproductive pairs (integer).
+#'   \item \code{Eyasses} the number of fledged young (integer).
+#' }
+#'
+#' @format A data frame with 40 rows and 4 columns
+#' @source Kery & Schaub (2011 p.64-65) courtesy of R.-J. Monneret
+#' @references
+#' Kery M & Schaub M (2011) Bayesian Population Analysis
+#' using WinBUGS. Academic Press. (\url{http://www.vogelwarte.ch/bpa})
+"peregrine"
