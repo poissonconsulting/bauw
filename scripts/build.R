0a6e098255396e7a5edc637b01f4482b898b762b
Joe Thorley <joe@poissonconsulting.ca>
2024-08-09 11:01:37 -0700

    build script

--- scripts/build.R
+++ scripts/build.R
@@ -1,5 +1,15 @@
 roxygen2md::roxygen2md()
-styler::style_pkg(filetype = c("R", "Rmd"))
+
+styler::style_pkg(
+  scope = "line_breaks",
+  filetype = c("R", "Rmd")
+)
+
+lintr::lint_package(linters = linters_with_defaults(
+  line_length_linter = line_length_linter(1000),
+  object_name_linter = object_name_linter(regexes = ".*"))
+)
+
 lintr::lint_package()
 
 devtools::test()
